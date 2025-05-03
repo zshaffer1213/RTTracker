ecd10f5dc91afec424773cd47bab8afa6c88e7d8
zshaffer1213 <zshaff1213@gmail.com>
2025-05-03 00:12:06 -0400

    pwa service worker

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'rt-tracker-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/index.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
